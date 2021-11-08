c2b837a7e04a8c84619e8a355c04df1d11ec9b08
MatiasTong <58445480+MatiasTong@users.noreply.github.com>
2021-11-07 21:53:13 -0500

    minor edits

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log(app.js)
+// let commandLineArgs = process.argv;
+// console.log(process)
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+// console.log(profileDataArgs)
+
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach( profileItem => console.log(profileItem));
+}
+
+
+printProfileData(profileDataArgs)
