746723d41b56b4fcb25141a48fbf20679a9c5a3d
MatiasTong <58445480+MatiasTong@users.noreply.github.com>
2021-11-28 19:02:37 -0500

    save recent progress

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const{ writeFile, copyFile} = require('./utils/generate-site.js');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -139,6 +139,21 @@ const promptProject = portfolioData => {
     )
   };
 
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => console.log(portfolioData))
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
